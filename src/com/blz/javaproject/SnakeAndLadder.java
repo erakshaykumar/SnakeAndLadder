ea298ad33e0071ce1e00381df6e80dc368730aa6
erakshaykumar <akaakshay304@gmail.com>
2022-02-07 09:23:27 +0530

    added: player position

--- src/com/blz/javaproject/SnakeAndLadder.java
+++ src/com/blz/javaproject/SnakeAndLadder.java
@@ -3,8 +3,7 @@ package com.blz.javaproject;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake And Ladder");
-
+		int player1Position = 0;
 	}
 
 }
