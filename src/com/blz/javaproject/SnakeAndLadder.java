2b588d3a500977634997f91b7664542026558dce
erakshaykumar <akaakshay304@gmail.com>
2022-02-07 14:48:14 +0530

    added: die roll logic

--- src/com/blz/javaproject/SnakeAndLadder.java
+++ src/com/blz/javaproject/SnakeAndLadder.java
@@ -4,6 +4,13 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int player1Position = 0;
+		
+        int player1Dice = (int)(Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println("Dice rolled value: " + player1Dice);
+		
 	}
-
+	
+	
 }
+     
+
